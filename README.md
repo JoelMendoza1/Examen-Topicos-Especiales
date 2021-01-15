c92747c4622a950866f15174be8c5c478752d0aa
Joel Mendoza <wester.mendoza@epn.edu.ec>
2021-01-15 11:54:08 -0500

    Chat, Login, Encriptar

---
+++ README.md
@@ -0,0 +1,15 @@
+#  LOGIN CON GMAIL USANDO IONIC & FIREBASE
+
+INTEGRANTES DE GRUPO  :man:
+- Mendoza Joel
+
+
+# README #
+
+Este README contiene un explicativo del la aplicación de login con E-mail que ingrese a la intefaz de chat y se pueda visualizar el chat de de los difentes usuarios esta aplicacion fue realizada con Ionic usando Angular y Firebase.La protección de los mensajes fue realizada con la función btoa() y la desencrptación con atob().
+
+## Acerca del proyecto ##
+### Explicación ###
+
+
+### Comandos usados
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAiaFFn7Dy6k-PY-4Bq6ADVMonz3pi-iXA",
+    authDomain: "examen-d80df.firebaseapp.com",
+    projectId: "examen-d80df",
+    storageBucket: "examen-d80df.appspot.com",
+    messagingSenderId: "54303265980",
+    appId: "1:54303265980:web:830a57deda06e4dcda2cb9",
+    measurementId: "G-97087CF89B"
+  }};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
